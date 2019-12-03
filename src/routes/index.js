bd59279bb9da5c96c467ffa11ff6346b8afa32a3
iamhassanmasood <iamhassanmasood@gmail.com>
2019-12-03 22:22:26 +0500

    Post request form

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,14 +6,20 @@
 
 router.post("/register", (req, res, next)=>{
   const user = new User({
-    email:"iamhassanmasood@gmail.com",
-    password:"test"
+    email:req.body.email,
+    password:req.body.password
   })
-  user.save()
-  .then(result =>{
-    console.log(result)
-    res.status(201).json({message:"New user added to db"})})
-  .catch((err) => next(err))
+  User.findOne({email:req.body.email}).then(u=>{
+    if(!u){
+      user.save()
+      .then(result =>{
+        console.log(result)
+        res.status(201).json({message:"New user added to db"})})
+      .catch((err) => next(err))
+    } else {
+      res.status(409).json({message: "User already exist with this email"})
+    }
+  })
 })
   
 
