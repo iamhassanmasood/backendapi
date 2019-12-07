d8e482b1e71382d27a591d02e67fee1de644c9d2
iamhassanmasood <iamhassanmasood@gmail.com>
2019-12-07 10:28:40 +0500

    login

--- src/routes/User/Login.js
+++ src/routes/User/Login.js
@@ -5,47 +5,26 @@ const bcrypt = require('bcryptjs');
 const jwt = ('jsonwebtoken')
 
 router.post("/login", (req, res, next) => {
-    User.find({ email: req.body.email })
-      .exec()
-      .then(user => {
-        if (user.length < 1) {
-          return res.status(401).json({
-            message: "Auth failed"
-          });
+    const payload = req.body;
+    User.findOne({email:payload.email})
+    .then(user=>{
+        if(!user){
+            res.status(401).json({message: "Auth Failed"})
+        } else {
+            bcrypt.compare(payload.password, user.password, function(err, results){
+                if(err){
+                    throw new Error(err)
+                 }
+                 if (results) {
+                    return res.status(200).json({ message: "Login success" })
+                } else {
+                    return res.status(401).json({ message: "Invalid credencial" })
+                }
+               })
+            }
         }
-        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-          if (err) {
-            return res.status(401).json({
-              message: "Auth failed"
-            });
-          }
-          if (result) {
-            const token = jwt.sign(
-              {
-                email: user[0].email,
-                userId: user[0]._id
-              },
-              process.env.JWT_KEY,
-              {
-                  expiresIn: "1h"
-              }
-            );
-            return res.status(200).json({
-              message: "Auth successful",
-              token: token
-            });
-          }
-          res.status(401).json({
-            message: "Auth failed"
-          });
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        });
-      });
+    )
+    .catch(err=> console.log(err))
   });
   
 module.exports = router;
