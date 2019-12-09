636e27c555d4a757390a1c9f57f41611b2233ff1
iamhassanmasood <iamhassanmasood@gmail.com>
2019-12-10 03:01:01 +0500

    Get rid of bcryptjs using cryto

--- src/routes/User/Login.js
+++ src/routes/User/Login.js
@@ -1,40 +1,27 @@
 const express = require('express')
 const router = express.Router();
 const User = require('../../models/User')
-const bcrypt = require('bcryptjs');
-const jwt = require("jsonwebtoken");
 
 router.post("/login", (req, res, next) => {
     const payload = req.body;
+
     User.findOne({email:payload.email})
     .then(user=>{
-        if(!user){
-            res.status(401).json({message: "Auth Failed"})
-        } else {
-            bcrypt.compare(payload.password, user.password, function(err, results){
-                if(err){
-                    throw new Error(err)
-                 }
-                 if (results) {
-                    const token = jwt.sign(
-                        {
-                          email: user.email,
-                          userId: user._id
-                        },
-                        process.env.JWT_KEY,
-                        {
-                            expiresIn: "1h"
-                        }
-                      );
-                    return res.status(200).json({ 
-                        message: "Login success", 
-                        token
-                    })
-                } else {
-                    return res.status(401).json({ message: "Invalid credencial" })
-                }
-               })
-            }
+        if(!user) res.status(401).json({message: "Auth Failed"})  
+        else {
+            if (user.validatePassword(req.body.password)) { 
+                const token = user.generateJWT()
+                return res.status(201).send({ 
+                    message: "Login success", 
+                    token
+                }) 
+            } else { 
+                return res.status(400).send({ 
+                    error : "Auth Failed"
+                }); 
+            } 
+           } 
+
         }
     )
     .catch(err=> console.log(err))
--- src/routes/User/Signup.js
+++ src/routes/User/Signup.js
@@ -1,27 +1,21 @@
 const express = require('express')
 const router = express.Router();
 const User = require('../../models/User')
-const bcrypt = require('bcryptjs');
 
 router.post("/signup", (req, res, next)=>{
  const payload = req.body;
- const user = new User({
+ const newUser = new User({
    username:payload.username,
    email:payload.email,
    password:payload.password
  })
- User.findOne({email:payload.email} && {username:payload.username}).then(u=>{
-   if(!u){
-    bcrypt.genSalt(10, function(err, salt) {
-      bcrypt.hash(payload.password, salt, function(err, hash) {
-        if(err) throw new Error(err)
-          user.password = hash
-          user.save()
-          .then(result =>{
-            res.status(201).json({message:"New user added to db", result})})
-          .catch((err) => next(err))
-      })
-  })
+ User.findOne({email:payload.email} && {username:payload.username}).then(user=>{
+   if(!user){
+      newUser.setPassword(payload.password)
+      newUser.save()
+      .then(result =>{
+        res.status(201).json({message:"New user added to db", result})})
+      .catch((err) => next(err))
    } else {
      res.status(409).json({message: "E-Mail or Username already exists"})
    }
